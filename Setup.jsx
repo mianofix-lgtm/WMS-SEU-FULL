dbf770a1ee3b6e467935ed51b012b5595d9508f9
mianofix-lgtm <mianofix@gmail.com>
2026-03-29 18:52:20 -0300

    Update Setup.jsx

--- Setup.jsx
+++ Setup.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { createUser } from '../firebase.js';
+import { createUser } from './firebase.js';
 import { Link } from 'react-router-dom';
 
 export default function Setup() {
